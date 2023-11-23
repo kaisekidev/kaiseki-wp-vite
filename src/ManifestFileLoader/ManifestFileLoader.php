111795f6effbde01e941a87cac14baf9eea9b792
David Mondok <3883758+davidmondok@users.noreply.github.com>
2023-11-23 13:05:36 +0100

    make ishot false default in manifest file loader

--- src/ManifestFileLoader/ManifestFileLoader.php
+++ src/ManifestFileLoader/ManifestFileLoader.php
@@ -8,7 +8,7 @@ use Kaiseki\WordPress\Vite\ViteServerInterface;
 
 final class ManifestFileLoader implements ManifestFileLoaderInterface
 {
-    public function __construct(private readonly string $path, private readonly bool $isHot)
+    public function __construct(private readonly string $path, private readonly bool $isHot = false)
     {
     }
 
