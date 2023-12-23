c6c95e651010aa94c4b64e4c7e8fa10a3fd192df
David Mondok <3883758+davidmondok@users.noreply.github.com>
2023-12-23 18:06:21 +0100

    Refactor ManifestFileLoader constructor to accept nullable isHot parameter

--- src/ManifestFileLoader/ManifestFileLoader.php
+++ src/ManifestFileLoader/ManifestFileLoader.php
@@ -8,12 +8,16 @@ use Kaiseki\WordPress\Vite\ViteServerInterface;
 
 final class ManifestFileLoader implements ManifestFileLoaderInterface
 {
-    public function __construct(private readonly string $path, private readonly bool $isHot = false)
+    public function __construct(private readonly string $path, private readonly ?bool $isHot = null)
     {
     }
 
     public function __invoke(ViteServerInterface $viteClient): ?string
     {
+        if ($this->isHot === null) {
+            return $this->path;
+        }
+
         return $viteClient->isHot() === $this->isHot ? $this->path : null;
     }
 }
